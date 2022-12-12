1dcd8b6d329247ec0666250236a615e3ddf12e59
safwanjmuhammed <safwanjmuhammed@gmail.com>
2022-12-12 19:30:19 +0530

    will

---
+++ lib/model/videos.dart
@@ -0,0 +1,12 @@
+class video {
+  String? thumbnail;
+  String? videotitle;
+  String? duration;
+  String? channel;
+  String? avatar;
+  String? views;
+  String? date;
+
+  video(this.thumbnail, this.videotitle, this.duration, this.channel,
+      this.avatar, this.views, this.date);
+}
