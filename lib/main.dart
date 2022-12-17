08a643702fb73652c0143e88f2a2a24c0d42724b
safwanjmuhammed <safwanjmuhammed@gmail.com>
2022-12-18 01:02:34 +0530

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,116 +1,101 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
-void main() {
-  runApp(MyApp());
-}
+import 'appbars.dart';
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(scaffoldBackgroundColor: Colors.black),
       debugShowCheckedModeBanner: false,
-      title: 'youtube',
-      home: homepage(),
+      theme: ThemeData.dark().copyWith(
+        accentColor: Colors.white,
+      ),
+      home: HomePage(),
     );
   }
 }
 
-class homepage extends StatefulWidget {
+class HomePage extends StatefulWidget {
   @override
-  _homepageState createState() => _homepageState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _homepageState extends State<homepage> {
+class _HomePageState extends State<HomePage> {
+  Widget eachvideo(String asset, String title) {
+    return Column(
+      children: <Widget>[
+        Stack(
+          children: <Widget>[
+            Image.asset(asset),
+            Positioned.fill(
+              bottom: 10.0,
+              right: 10.0,
+              child: Align(
+                alignment: Alignment.bottomRight,
+                child: Container(
+                  color: Colors.black,
+                  padding: EdgeInsets.all(4.0),
+                  child: Text(
+                    "20:10",
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+        SizedBox(
+          height: 5.0,
+        ),
+        Container(
+          color: Colors.black,
+          child: ListTile(
+            leading: CircleAvatar(
+              backgroundImage: AssetImage('assests/avatar.jpg'),
+            ),
+            title: Text(
+              "$title",
+              textAlign: TextAlign.start,
+            ),
+            subtitle: Text(
+              "Navigation bar - Flutter widget of the week",
+            ),
+            trailing: Icon(Icons.more_vert, color: Colors.white),
+          ),
+        ),
+        SizedBox(
+          height: 10.0,
+          child: Container(
+            color: Colors.black,
+          ),
+        )
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.black,
-          //elevation: 0,
-          title: Image.asset(
-            'images/youtube.png',
-            height: 125,
-            width: 200,
-            //alignment: Alignment.centerLeft,
-          ),
-          actions: [
-            IconButton(onPressed: () {}, icon: Icon(Icons.cast)),
-            IconButton(onPressed: () {}, icon: Icon(Icons.notifications)),
-            IconButton(onPressed: () {}, icon: Icon(Icons.search)),
-            CircleAvatar(
-              radius: 16,
-              backgroundImage: AssetImage("images/avatar.jpeg"),
-            )
+      appBar: topbar,
+      bottomNavigationBar: bottomappbar,
+      backgroundColor: Colors.black,
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            eachvideo("assests/images/images.png",
+                "Working With Databases In Flutter"),
+            eachvideo("assests/images/images.png",
+                "Working With Databases In Flutter"),
+            eachvideo("assests/images/images.png",
+                "Working With Databases In Flutter"),
+            eachvideo("assests/images/images.png",
+                "Working With Databases In Flutter"),
+            eachvideo(
+                "assests/thumb4.png", "Working With Databases In Flutter"),
           ],
         ),
-
-        //body
-        body: SingleChildScrollView(
-            child: Column(
-          children: <Widget>[
-            Stack(children: <Widget>[
-              Image.asset(
-                "images/images.png",
-              ),
-              Positioned.fill(
-                  bottom: 10,
-                  right: 10,
-                  child: Align(
-                      alignment: Alignment.bottomRight,
-                      child: Container(
-                          padding: EdgeInsets.all(4.0),
-                          child: Text("7:8"),
-                          color: Colors.white)))
-            ]),
-
-            //
-            SizedBox(
-                height: 60.0,
-                child: ListTile(
-                  leading: CircleAvatar(
-                    backgroundImage: AssetImage('images/flutter.png'),
-                  ),
-                  textColor: Colors.white,
-                  title: Text('Navitation bar - Flutter widget of the week'),
-                  subtitle: Text(
-                    'flutter . 17k views. 1 day ago',
-                    selectionColor: Colors.grey,
-                  ),
-                ))
-          ],
-        )),
-        bottomNavigationBar: BottomNavigationBar(
-            //unselectedItemColor: Colors.white,
-            //selectedItemColor: Colors.white,
-            items: [
-              BottomNavigationBarItem(
-                icon: Icon(
-                  Icons.home,
-                ),
-                label: "Home",
-                backgroundColor: Colors.black,
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.explore),
-                label: "Explore",
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.add_circle_outline),
-                label: 'Add',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.subscriptions),
-                label: 'Subscriptions',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.video_file),
-                label: 'Library',
-              ),
-            ]));
+      ),
+    );
   }
 }
-//testsync
